d400cafd750f69da04eee96327e5fdd6357cfb75
aryatam <arya81tam@gmail.com>
2023-11-22 18:18:12 +0330

    test

--- compiler.py
+++ compiler.py
@@ -1,6 +1,38 @@
+import string
+from enum import Enum
+from typing import List, Optional, Set, Dict, Tuple
+
+
+class Scanner:
+    _EOF = None
+    _all_chars: Set[str] = set(chr(i) for i in range(128))
+    _digits: Set[str] = set(string.digits)
+    _letters: Set[str] = set(string.ascii_letters)
+    _alphanumerics: Set[str] = _digits.union(_letters)
+    _symbols: Set[str] = {';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '/', '=', '<'}
+    _whitespaces: Set[str] = {' ', '\n', '\r', '\t', '\v', '\f'}
+    _valid_chars: Set[str] = _alphanumerics.union(_symbols, _whitespaces)
+    _keywords = {"if", "else", "void", "int", "while", "break", "switch", "default", "case", "return", "endif"}
+
+    NUM: str = "NUM"
+    ID: str = "ID"
+    KEYWORD: str = "KEYWORD"
+    SYMBOL: str = "SYMBOL"
+    COMMENT: str = "COMMENT"
+    WHITESPACE: str = "WHITESPACE"
+
+    def __int__(self, buffer_size=1024):
+        self._input_file = open("input.txt", mode="r")
+        print(self._input_file)
+        self._buffer_size = buffer_size
+        self._buffer: List[Optional[str]] = []
+        self._token_buffer: List[str] = []
+
+
+
 class Compiler:
     def __init__(self):
-        pass
+        scanner = Scanner()
 
 
 if __name__ == '__main__':
