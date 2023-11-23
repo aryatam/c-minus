d4890d153b3f6f74a7d7474e0b18e1e9972324f6
aryatam <arya81tam@gmail.com>
2023-11-23 14:40:34 +0330

    states are complete!

--- compiler.py
+++ compiler.py
@@ -38,6 +38,7 @@ class Scanner:
     whitespaces: Set[str] = {' ', '\n', '\r', '\t', '\v', '\f'}
     valid_chars: Set[str] = alphanumerics.union(symbols, whitespaces)
     keywords = {"if", "else", "void", "int", "while", "break", "return"}
+    end: Set[str] = symbols.union(whitespaces, {EOF})
 
     def __init__(self):
         self.transitions: list[Transition] = []
@@ -49,18 +50,79 @@ class Scanner:
             self.state.append(State(i))
 
     def generateTransitons(self):
+        # start state 0 Start
         self.state[0].listTransiton.append(Transition(self.state[0], self.state[1], list(self.digits)))
-        self.state[1].listTransiton.append(Transition(self.state[1], self.state[1], list(self.digits)))
-        self.state[1].isFinal = True
+        self.state[0].listTransiton.append(Transition(self.state[0], self.state[2], list(self.letters)))
+        self.state[0].listTransiton.append(Transition(self.state[0], self.state[3],
+                                                      list(self.symbols - {'/', '=', '*'})))
+        self.state[0].listTransiton.append(Transition(self.state[0], self.state[4], list('=')))
+        self.state[0].listTransiton.append(Transition(self.state[0], self.state[5], list('/')))
+        self.state[0].listTransiton.append(Transition(self.state[0], self.state[6], list(self.whitespaces)))
+        self.state[0].listTransiton.append(Transition(self.state[0], self.state[7], list('*')))
 
-    def createTransitons(self):
-        pass
+        # state 1 NUM
+        self.state[1].listTransiton.append(Transition(self.state[1], self.state[1], list(self.digits)))
+        self.state[1].listTransiton.append(Transition(self.state[1], self.state[8], list(self.end)))
+
+        # state 2 ID
+        self.state[2].listTransiton.append(Transition(self.state[2], self.state[2], list(self.alphanumerics)))
+        self.state[2].listTransiton.append(Transition(self.state[2], self.state[9], list(self.end)))
+
+        # state 3 symbol - lookahead
+
+        # state 4 = , ==
+        self.state[4].listTransiton.append(Transition(self.state[4], self.state[10], list('=')))
+        self.state[4].listTransiton.append(Transition(self.state[4], self.state[11],
+                                                      list(self.valid_chars.union(self.EOF - '='))))
+        # state 5 / /* //
+        self.state[5].listTransiton.append(Transition(self.state[5], self.state[12],
+                                                      list(self.valid_chars.union(self.EOF - '*', '/'))))
+        self.state[5].listTransiton.append(Transition(self.state[5], self.state[13], list('/')))
+        self.state[5].listTransiton.append(Transition(self.state[5], self.state[14], list('*')))
+
+        # state 13 //
+        self.state[13].listTransiton.append(Transition(self.state[13], self.state[13], list(self.all_chars - '\n')))
+        self.state[13].listTransiton.append(Transition(self.state[13], self.state[15], list("/n" + self.EOF)))
+
+        # state 14 /*
+        self.state[14].listTransiton.append(Transition(self.state[14], self.state[14], list(self.all_chars - '*')))
+        self.state[14].listTransiton.append(Transition(self.state[14], self.state[16], list('*')))
+
+        # state 16 */
+        self.state[16].listTransiton.append(Transition(self.state[16], self.state[17], list('/')))
+        self.state[16].listTransiton.append(Transition(self.state[14], self.state[16], list(self.all_chars - '/')))
+
+        # state 7 * the only problem is /* without /*
+        self.state[7].listTransiton.append(Transition(self.state[7], self.state[18], list(self.all_chars - '/')))
+
+    def setFinal_lookahead(self):
+        # final stages of DFA
+
+        self.state[6].isFinal = True
+        self.state[8].isFinal = True
+        self.state[9].isFinal = True
+        self.state[3].isFinal = True
+        self.state[11].isFinal = True
+        self.state[10].isFinal = True
+        self.state[12].isFinal = True
+        self.state[15].isFinal = True
+        self.state[17].isFinal = True
+        self.state[18].isFinal = True
+
+        # read the input but don't use it its look ahead
+
+        self.state[8].isLookAhead = True
+        self.state[9].isLookAhead = True
+        self.state[11].isLookAhead = True
+        self.state[12].isLookAhead = True
+        self.state[15].isLookAhead = True
+        self.state[18].isLookAhead = True
 
 
 class Compiler:
 
     def __init__(self):
-        scanner = Scanner()
+        pass
 
 
 if __name__ == '__main__':
